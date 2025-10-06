7f5541dcd93edad4fa77ae9cd5d559beaa65dd39
AlphaXen <kimminjae771@naver.com>
2025-10-07 02:35:30 +0900

    feat(setting_screen): Add comment to explain threshold purpose

--- lib/week04/random_dice/screen/setting_screen.dart
+++ lib/week04/random_dice/screen/setting_screen.dart
@@ -2,7 +2,7 @@ import 'package:myapp/week04/random_dice/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatelessWidget {
-  final double threshold;
+  final double threshold; // Slider의 현잿값
 
   final ValueChanged<double> onThresholdChange;
 
