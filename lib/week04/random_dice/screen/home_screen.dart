673a1724b9c286dcdc1c480f4daebbf10ea212a0
AlphaXen <kimminjae771@naver.com>
2025-10-07 02:24:01 +0900

    feat(random_dice): Require number parameter in HomeScreen constructor

--- lib/week04/random_dice/screen/home_screen.dart
+++ lib/week04/random_dice/screen/home_screen.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   final int number;
 
-  const HomeScreen({super.key});
+  const HomeScreen({
+    required this.number,
+    Key? key,
+  });
 
   @override
   Widget build(BuildContext context) {
