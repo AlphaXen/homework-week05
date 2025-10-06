df2a2aec2970dd486fc0214c4491987a033e893e
AlphaXen <kimminjae771@naver.com>
2025-10-07 02:36:31 +0900

    feat(setting_screen): Accept and use onThresholdChange callback.

--- lib/week04/random_dice/screen/setting_screen.dart
+++ lib/week04/random_dice/screen/setting_screen.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 class SettingScreen extends StatelessWidget {
   final double threshold; // Slider의 현잿값
 
+  // Slider가 변경될 때마다 실행되는 함수
   final ValueChanged<double> onThresholdChange;
 
   const SettingScreen({
     Key? key,
 
+    // threshold와 onThresholdChange는 SettingScreen에서 입력
     required this.threshold,
     required this.onThresholdChange,
   }) : super(key: key);
