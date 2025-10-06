368e96952da1c02e4530bddac3c26c39afb5a466
AlphaXen <kimminjae771@naver.com>
2025-10-07 01:02:27 +0900

    feat: Implement custom theme for Random Dice app

--- lib/week04/random_dice/main.dart
+++ lib/week04/random_dice/main.dart
@@ -1,9 +1,24 @@
+import 'package:myapp/week04/random_dice/const/colors.dart';
 import 'package:myapp/week04/random_dice/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroudColor,
+        sliderTheme: SliderThemeData(
+          thumbColor: primaryColor,
+          activeTickMarkColor: primaryColor,
+
+          inactiveTrackColor: primaryColor.withOpacity(0.3),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: primaryColor,
+          unselectedItemColor: secondaryColor,
+          backgroundColor: backgroudColor,
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
