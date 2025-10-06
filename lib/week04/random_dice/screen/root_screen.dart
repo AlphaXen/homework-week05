e7a3fd2d37aa0be2afe4725a511e71fe61d05386
AlphaXen <kimminjae771@naver.com>
2025-10-07 01:57:30 +0900

    feat: Implement basic RootScreen layout

--- lib/week04/random_dice/screen/root_screen.dart
+++ lib/week04/random_dice/screen/root_screen.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+class RootScreen extends StatelessWidget {
+  const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        children: renderChildren(),
+      ),
+    )
+  }
+}
