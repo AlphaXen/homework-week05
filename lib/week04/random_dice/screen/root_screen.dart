e02ba6add02e1dbfe16ac9b0f62180c89e236d04
AlphaXen <kimminjae771@naver.com>
2025-10-07 02:31:31 +0900

    feat(dice): Remove placeholder tab and start settings screen

--- lib/week04/random_dice/screen/root_screen.dart
+++ lib/week04/random_dice/screen/root_screen.dart
@@ -47,16 +47,7 @@ TickerProviderStateMixin{ // TickerProviderStateMixin 사용하기
 
   List<Widget> renderChildren() {
     return [
-      Container( // 홈 탭
-        child: Center(
-          child: Text(
-            'Tab 1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
+      
 
       Container( // 설정 스크린 탭
         child: Center(
