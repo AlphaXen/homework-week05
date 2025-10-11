df17157b454bc761df7234b4f97cd26bc6082cc3
AlphaXen <kimminjae771@naver.com>
2025-10-11 18:06:45 +0900

    feat: Implement basic ScheduleBottomSheet widget

---
+++ lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScheduleBottomSheet extends StatefulWidget {
+  const ScheduleBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  State<ScheduleBottomSheet> createState() => _ScheduleBottomState();
+}
+
+class _ScheduleBottomSheetState
