62db095b971136789cb4236e0a4c7610854c4235
AlphaXen <kimminjae771@naver.com>
2025-10-11 17:49:45 +0900

    feat: Implement basic TodayBanner component

---
+++ lib/week05/calendar_scheduler/component/today_banner.dart
@@ -0,0 +1,18 @@
+import 'package:myapp/week05/calendar_scheduler/const/color.dart';
+import 'package:flutter/material.dart';
+
+class TodayBanner extends StatelessWidget {
+  final DateTime selectedDate;
+  final int count;
+
+  const TodayBanner({
+    required this.selectedDate,
+    required this. count,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
