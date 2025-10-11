8634de1b07a9f40b9992954be917f1ac436ef06b
AlphaXen <kimminjae771@naver.com>
2025-10-11 13:51:20 +0900

    feat(calendar): center calendar header title

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -9,6 +9,9 @@ class MainCalendar extends StatelessWidget {
       firstDay: DateTime(1800, 1, 1), // 첫째 날
       lastDay: DateTime(3000, 1, 1), // 마지막 날
       focusDay: DateTime.now(), // 화면에 보여지는 날
+      headerStyle: HeaderStyle(
+        titleCentered: true
+      )
     );
   }
 }
