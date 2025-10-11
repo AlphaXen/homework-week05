ce0fabc868e7daaa32bcd6f54a681447ebb74805
AlphaXen <kimminjae771@naver.com>
2025-10-11 17:46:44 +0900

    feat: Add sample ScheduleCard to HomeScreen

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -29,6 +29,11 @@ class _HomeScreen extends State<HomeScreen> {
             // 날짜가 선택되었을 때 실행할 함수
               onDaySelected: onDaySelected, // 선택된 날짜
             ),
+            ScheduleCard( // 구현해둔 일정 카드
+              startTime: 12,
+              endTime: 14,
+              content: '프로그래밍 공부',
+            ),
           ],
         ),
       ),
