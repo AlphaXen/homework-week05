16e117dfcfe69f92b5f8318e929d7a8aeaa7a1e5
AlphaXen <kimminjae771@naver.com>
2025-10-11 18:10:45 +0900

    feat: Import ScheduleBottomSheet and Colors

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:myapp/week05/calendar_scheduler/component/schedule_card.dart';
 import 'package:myapp/week05/calendar_scheduler/component/today_banner.dart';
-import 'package:myapp/week05/calendar_scheduler/component/'
+import 'package:myapp/week05/calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:myapp/week05/calendar_scheduler/const/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
