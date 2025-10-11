ab77d4b7c6d20a489e7c7a7174ae00cd76a49a31
AlphaXen <kimminjae771@naver.com>
2025-10-11 17:09:14 +0900

    feat: Add comments to MainCalendar for clarity

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -3,8 +3,8 @@ import 'package:myapp/week05/calendar_scheduler/const/color.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class MainCalendar extends StatelessWidget {
-  final OnDaySelected onDaySelected;
-  final DateTime selectedDate;
+  final OnDaySelected onDaySelected; // 날짜 선택 시 실행할 함수
+  final DateTime selectedDate; // 선택된 날짜
 
   MainCalendar({
     required this.onDaySelected,
