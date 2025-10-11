157cc07681a53d03a083ea8ffca84a5169b5b128
AlphaXen <kimminjae771@naver.com>
2025-10-11 17:09:44 +0900

    feat: Add onDaySelected callback comment to MainCalendar

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -15,6 +15,8 @@ class MainCalendar extends StatelessWidget {
   Widget build(BuildContext context) {
     return TableCalendar(
       onDaySelected: onDaySelected,
+      // 날짜 선택 시 실행할 함수
+      
       firstDay: DateTime(1800, 1, 1), // 첫째 날
       lastDay: DateTime(3000, 1, 1), // 마지막 날
       focusedDay: DateTime.now(), // 화면에 보여지는 날
