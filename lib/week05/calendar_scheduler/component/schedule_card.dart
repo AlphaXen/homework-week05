0163e66a0d911fc970a20d18707820a87fd2e59a
AlphaXen <kimminjae771@naver.com>
2025-10-11 17:27:15 +0900

    feat: Implement basic ScheduleCard component

---
+++ lib/week05/calendar_scheduler/component/schedule_card.dart
@@ -0,0 +1,18 @@
+import 'package:myapp/week05/calendar_scheduler/const/color.dart';
+import 'package:flutter/material.dart';
+
+class _Time extends StatelessWidget {
+  final int startTime;
+  final int endTime;
+
+  const _Time({
+    required this.startTime,
+    required this.endTime,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
