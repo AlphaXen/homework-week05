ef4e459e1ba753a08b9401510ea852a1107ae5b0
AlphaXen <kimminjae771@naver.com>
2025-10-11 17:51:44 +0900

    feat(calendar): Implement basic TodayBanner UI

--- lib/week05/calendar_scheduler/component/today_banner.dart
+++ lib/week05/calendar_scheduler/component/today_banner.dart
@@ -13,6 +13,19 @@ class TodayBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    );
+
+    return Container(
+      color: PRIMARY_COLOR,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: ,
+        )
+      ),
+    )
   }
 }
