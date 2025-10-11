c4878cc8942041daf8c3a2b6096a623c04830094
AlphaXen <kimminjae771@naver.com>
2025-10-11 18:42:15 +0900

    Fix: Adjust bottom sheet height to account for keyboard

--- lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -14,13 +14,13 @@ class _ScheduleBottomState extends State<ScheduleBottomSheet> {
   Widget build(BuildContext context) {
     // 키보드높이 가져오기
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
-    
+
     return SafeArea(
       child: Container(
-        height: MediaQuery.of(context).size.height /2,
+        height: MediaQuery.of(context).size.height /2 + bottomInset,
         color: Colors.white,
         child: Padding(
-          padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+          padding: EdgeInsets.only(left: 8, right: 8, top: 8),
           child: Column(
             // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치
             children: [
