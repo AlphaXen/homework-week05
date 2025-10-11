5f2a58ff5757a73a7838ad939c5ceaf5b28736d4
AlphaXen <kimminjae771@naver.com>
2025-10-11 17:11:16 +0900

    feat(calendar): Highlight selected day in MainCalendar

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -16,7 +16,10 @@ class MainCalendar extends StatelessWidget {
     return TableCalendar(
       onDaySelected: onDaySelected,
       // 날짜 선택 시 실행할 함수
-      
+      selectedDayPredicate: (date) => // 선택된 날짜를 구분할 로직
+        date.year == selectedDate.year &&
+        date.month == selectedDate.month &&
+        date.day == selectedDate.day,
       firstDay: DateTime(1800, 1, 1), // 첫째 날
       lastDay: DateTime(3000, 1, 1), // 마지막 날
       focusedDay: DateTime.now(), // 화면에 보여지는 날
