b77760327337807fcc9c6ab6ca2d53c968534cbd
AlphaXen <kimminjae771@naver.com>
2025-10-11 17:04:43 +0900

    Fix: Corrected typo in `focusDay` to `focusedDay` and styled calendar.

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -10,7 +10,7 @@ class MainCalendar extends StatelessWidget {
     return TableCalendar(
       firstDay: DateTime(1800, 1, 1), // 첫째 날
       lastDay: DateTime(3000, 1, 1), // 마지막 날
-      focusDay: DateTime.now(), // 화면에 보여지는 날
+      focusedDay: DateTime.now(), // 화면에 보여지는 날
       headerStyle: HeaderStyle(
         titleCentered: true,
         formatButtonVisible: false,
@@ -31,7 +31,24 @@ class MainCalendar extends StatelessWidget {
         ),
         selectedDecoration: BoxDecoration(
           borderRadius: BorderRadius.circular(6.0),
-        )
+          border: Border.all(
+            color: PRIMARY_COLOR,
+            width: 1.0,
+          ),
+        ),
+        defaultTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: DARK_GREY_COLOR,
+        ),
+        weekendTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: DARK_GREY_COLOR,
+        ),
+        selectedTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: PRIMARY_COLOR,
+        ),
+      ),
     );
   }
 }
