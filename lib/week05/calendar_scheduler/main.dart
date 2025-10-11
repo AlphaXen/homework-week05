a8824f10ed4de06230be567682f460f8665ff477
AlphaXen <kimminjae771@naver.com>
2025-10-11 18:44:45 +0900

    Fix: Import missing date_symbol_data_local for intl

--- lib/week05/calendar_scheduler/main.dart
+++ lib/week05/calendar_scheduler/main.dart
@@ -1,6 +1,6 @@
 import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/'
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
   runApp(
