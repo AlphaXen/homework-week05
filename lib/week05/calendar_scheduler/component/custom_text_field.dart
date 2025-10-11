c77b53b68038537b8d9f76a81f111c0337181aae
AlphaXen <kimminjae771@naver.com>
2025-10-11 18:22:45 +0900

    feat: Add comments explaining label and isTime in CustomTextField

--- lib/week05/calendar_scheduler/component/custom_text_field.dart
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
-  final String label;
-  final bool isTime;
+  final String label; // 텍스트 필드 제목
+  final bool isTime; // 시간 선택하는 텍스트 필드인지 여부
 
   const CustomTextField({
     required this.label,
+    required this.isTime,
     Key? key,
   }) : super(key: key);
 
